9fbc5f5e54c7ac239ef805f278bd737d47fd1fb2
Andjuel <mayanfang007@yeah.net>
2018-05-24 19:32:25 +0800

    add fate something

---
+++ js/fate.js
@@ -0,0 +1,11 @@
+/*
+ * js for fate.
+ */
+ var init = function a() {
+/*    var month_div = ele('#monthUl');
+    for (var i = 0 ; i < 12; i++) {
+        var dom = document.createElement('li');
+        dom.appendChild(document.createTextNode(MONTH_NAME[i]));
+        month_div.appendChild(dom);
+    }*/
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 /*
  * This would be the main JS file.
+ * 
  */
+const MONTH_NAME = ['正月', '二月','三月','四月','五月','六月','七月','八月','九月', '十月', '冬月','腊月'];
+function ele(key) {
+    if (isBlank(key))
+        return null;
+    if (key.indexOf('#') == 0) {
+        return window.document.getElementById(key.substr(1));
+    }
+}
+
+function isBlank (val) {
+    return undefined == val || null == val || '' == val;
+}
